842c25386d30c6e024944460591b6a45a69eaf0b
paarth <paarth.dandiwala@gmail.com>
2012-03-04 22:58:43 +0530

    backtrack for one possible LCS added

--- Dynamic_Programming/LCS/LCS.cpp
+++ Dynamic_Programming/LCS/LCS.cpp
@@ -10,6 +10,30 @@
 
 using namespace std;
 
+int n,m;
+
+int dp[1005][1005];
+
+string backtrack(string A,string B,int i,int j)
+{
+  if(i==0||j==0)
+    return "";
+  else if(A[i-1]==B[j-1])
+    {
+      return backtrack(A,B,i-1,j-1)+A[i-1];
+    }
+  else
+    {
+      if(dp[i][j-1]>dp[i-1][j])
+	return backtrack(A,B,i,j-1);
+      else
+	return backtrack(A,B,i-1,j);
+    }
+  
+}
+
+
+
 int main()
 {
 
@@ -19,16 +43,14 @@ int main()
 
   cout<<":Enter the first string: ";
   cin>>A;
-  int n=A.size();
+  n=A.size();
   
   cout<<"Enter the second string: ";
   cin>>B;
-  int m=B.size();
+  m=B.size();
 
   /////....inputs done :)
-  
-  int dp[n+2][m+2];//....'n+2' and 'm+2' :extra space jus 2 avoid errors
-
+    
   for(int i=0;i<=n;i++)
     {
       dp[i][0]=0;
@@ -55,7 +77,7 @@ cout<<"Length of the LCS is : "<<dp[n][m]<<"\n";
   
   
   
-
+ cout<<"One of the LCs may be : "<<backtrack(A,B,n,m)<<"\n";
 
 
 
