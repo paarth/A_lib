bb71684c87c17fcb9c79d16e76ab224be8f2fecf
paarth <paarth.dandiwala@gmail.com>
2012-03-11 04:14:45 +0530

    Kadane_s algo--max sub-array sum

---
+++ Dynamic_Programming/KADANE_ALGO/MAXIMUM_SUBARRAY_SUM.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<map>
+#include<set>
+#include<algorithm>
+#include<cstring>
+#include<climits>
+#include<vector>
+#include<cstdio>
+#include<stack>
+#include<queue>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"Enter the no. of elements: ";
+  scanf("%d",&n);
+
+  int a[n];
+  cout<<"Enter "<<n<<" Elements:\n";
+
+  for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+
+  long long buf=0;
+  long long res=(long long)INT_MIN;
+
+  for(int i=0;i<n;i++)
+    {
+      buf+=a[i];
+      if(buf<0)
+	buf=0;
+      else
+	res=max(buf,res);
+    }
+
+  cout<<"Answer: "<<res<<"\n";
+
+
+
+  return 0;
+}
