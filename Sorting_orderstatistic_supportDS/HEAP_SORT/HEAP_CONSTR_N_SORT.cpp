f604a5d0b9a0e3cde115310c54d00ec2fac028c0
paarth <paarth.dandiwala@gmail.com>
2012-05-31 15:47:46 +0530

    Heap_sort_using_rpeated_insertion_and_then_min_extraction_

--- Sorting_orderstatistic_supportDS/HEAP_SORT/HEAP_CONSTR_N_SORT.cpp
+++ Sorting_orderstatistic_supportDS/HEAP_SORT/HEAP_CONSTR_N_SORT.cpp
@@ -9,8 +9,8 @@
 #include<stack>
 
 using namespace std;
-#define pq_size 100000
-
+#define pq_size 100000   // maximum  priority_queue size
+ 
 
 struct pq{
   int q[pq_size+1];  // This shows the priority queue and the max buffer size.
@@ -28,7 +28,7 @@ int pq_young_child(int n)
   return (2*n);  // This returns left child index
 }
 
-void pq_swap(pq *q,int a,int b)
+void pq_swap(pq *q,int a,int b)   // This function is simply to swap [a] and [b] of the pq
 {
   int temp=q->q[a];
   q->q[a]=q->q[b];
@@ -40,10 +40,10 @@ void bubble_up(pq *q,int p)
   if(pq_parent(p)==-1) return;
   // i.e if the bubble up operation now presently at root// NOP further
 
-  if(q->q[pq_parent(p)]>q->q[p])
+  if(q->q[pq_parent(p)]>q->q[p])   // If the parent value is greater (needs bubble up op)
     {
-      pq_swap(q,p,pq_parent(p));
-      bubble_up(q,pq_parent(p));
+      pq_swap(q,p,pq_parent(p));  // make a swap 
+      bubble_up(q,pq_parent(p));  // again make the bubble up to satisfy and maintain heap property
     }
 }
 
@@ -56,20 +56,20 @@ void pq_insert(pq *q,int x)  // to be inserted in prio_queue:"q" and to be inser
     {
       q->n = (q->n)+1;  // incrementing the no. of queue elements as the insertion possible.
       q->q[q->n]=x;
-      bubble_up(q,q->n);
+      bubble_up(q,q->n);  // make bubble up to satisfy and maintain heap property
     }
 }
 
-void pq_init(pq *q)
+void pq_init(pq *q)  // Initialize the pq by no. of elements =0
 {
   q->n=0;
 }
 
-void make_heap(pq *q,int store[],int n)
+void make_heap(pq *q,int store[],int n) // parameters: pq , array to be sorted, size of array
 {
 
-  pq_init(q);
-  for(int i=0;i<n;i++)
+  pq_init(q);   
+  for(int i=0;i<n;i++)        // repeatedly and sequencially insert the array elements.
     pq_insert(q,store[i]);
 }
 
@@ -79,19 +79,20 @@ void bubble_down(pq *q,int p)
   int c;  // child index
   int min_index;  // index of lightest child
 
-  c=pq_young_child(p);
-  min_index=p;
+  c=pq_young_child(p); // child index given as the left child (and +1 will access right child)
+  min_index=p; 
 
-  for(int i=0;i<=1;i++)
+  for(int i=0;i<=1;i++)  // for both left and right child
     {
-      if((c+i)<=q->n)
+      if((c+i)<=q->n)    // get the index of the minimum of node and its both childrens
 	{
 	  if(q->q[min_index]>q->q[c+i])
 	    min_index=c+i;
 	}
     }
-
-      if(min_index!=p)
+  
+  // if need for swap ....swap and thus make bubble down recursively 
+      if(min_index!=p)     
 	{
 	  pq_swap(q,p,min_index);
 	  bubble_down(q,min_index);
@@ -104,13 +105,13 @@ int extract_min(pq *q)
 {
   int min=-1;
 
-  if(q->n<=0)
+  if(q->n<=0)   // if queue is empty
     cout<<"Warning: queue is empty... extract_min operation failure. Further NOP";
   else
     {
-      min=q->q[1];
-      q->q[1]=q->q[q->n];
-      q->n=(q->n)-1;
+      min=q->q[1];   // q[1] gives the min value.
+      q->q[1]=q->q[q->n];   // get any value say ..last and thus maintain property by bubble down   
+      q->n=(q->n)-1; 
       bubble_down(q,1);
     }
   return min;
@@ -120,9 +121,9 @@ void heap_sort(int store[],int n)
 {
   pq q;  // heap for HEAP_SORT
 
-  make_heap(&q,store,n);
+  make_heap(&q,store,n);   // construct heap from the array to be sorted
 
-  for(int i=0;i<n;i++)
+  for(int i=0;i<n;i++)     // extract min repeatedly to get elements in sorted order
     store[i]=extract_min(&q);
 }
 
