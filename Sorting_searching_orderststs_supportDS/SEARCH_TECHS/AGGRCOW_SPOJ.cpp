38585d4ca31aa05aea79f4d314d6e46a4f37843d
paarth <paarth.dandiwala@gmail.com>
2012-06-08 02:27:10 +0530

    example: Discrete binary search as a SPOJ problem

---
+++ Sorting_searching_orderststs_supportDS/SEARCH_TECHS/AGGRCOW_SPOJ.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<cstdio>
+#include<vector>
+#include<queue>
+#include<cmath>
+
+using namespace std;
+
+#define MAX 1000050
+
+int s[MAX];
+
+int possible(int d,int n,int c)
+{
+  int cnt=1;
+  for(int b=0,i=1;i<n&&cnt<c;i++)
+    {
+      if(s[i]-s[b]>=d) {cnt++;b=i;}
+    }
+  if(cnt>=c)
+    return 1;
+  
+  return 0;
+}
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+
+  int n,c;
+ 
+  for(int tt=0;tt<t;tt++)
+    {
+
+      scanf("%d %d",&n,&c);
+
+      for(int i=0;i<n;i++)
+	scanf("%d",&s[i]);
+
+      //.....sorting the stall points:
+      sort(s,s+n);
+
+      int lo=1,hi=s[n-1]-s[0]+1;
+      int mid;
+
+      while(lo<hi)
+	{
+	  mid=(lo+hi)/2;
+	  if(possible(mid,n,c))
+	    lo=mid+1;
+	  else
+	    hi=mid;
+	}
+
+      printf("%d\n",lo-!possible(lo,n,c));
+    }
+
+
+
+  return 0;
+}
