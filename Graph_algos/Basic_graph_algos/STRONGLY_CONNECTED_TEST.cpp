8760fd1ee187d0d399637b888cca5cd00a0034b6
paarth <paarth.dandiwala@gmail.com>
2012-06-26 02:58:12 +0530

    testing strongly connected graph_

--- Graph_algos/Basic_graph_algos/STRONGLY_CONNECTED_TEST.cpp
+++ Graph_algos/Basic_graph_algos/STRONGLY_CONNECTED_TEST.cpp
@@ -14,14 +14,14 @@ using namespace std;
 int n;
 vector<int> ck;
 
-void dfs(vector< vector<int> > edge,int node)
+void dfs(vector< vector<int> > edge,int node) // simple dfs 
 {
 
-  if(ck[node]==0)
+  if(ck[node]==0)  // if the node is not yet visited
     {
-      ck[node]=1;
+      ck[node]=1;  // mark it visited
 
-      for(int i=0;i<edge[node].size();i++)
+      for(int i=0;i<edge[node].size();i++)  // loop for all adjacent nodes and do dfs
 	dfs(edge,edge[node][i]);
 
     }
@@ -29,18 +29,18 @@ void dfs(vector< vector<int> > edge,int node)
 }
 
 
-int check(vector< vector<int> > edge)
+int check(vector< vector<int> > edge)  // To check according to the parameter edge: is the graph connected: all nodes can be reahced from any node say 0
 {
   ck.resize(n+1);
 
-  for(int i=0;i<n;i++)
+  for(int i=0;i<n;i++)  // mark all not visited initially
     ck[i]=0;
 
-  dfs(edge,0);
+  dfs(edge,0); // dfs to visit all reachable nodes
 
   int flag=1;
-  for(int i=0;i<n;i++)
-    if(ck[i]==0)
+  for(int i=0;i<n;i++)  // check if any of the nodes are not visited
+    if(ck[i]==0)  // if such node found thus the check fails and thus flag is set to 0 for return
       {flag=0;break;}
 
   return flag;
@@ -52,7 +52,7 @@ int main()
   cout<<"Enter the no. of nodes: ";
   cin>>n;
 
-  vector< vector<int> > edge,redge;
+  vector< vector<int> > edge,redge; // edge: edges and redge: reverse edges    i.e. if a -> b is there in edge then b-> a will be there in redge
   edge.resize(n+1);
   redge.resize(n+1);
 
@@ -72,16 +72,16 @@ int main()
 
 
 
-  int c1=check(edge);
+  int c1=check(edge);   // check if all are connected according to the edges given for the graph : making dfs internally
   int res=0;
 
   if (c1==0)
     cout<<"c1 itself is 0: means the graph is note even connected<unidectly>\n";
 
-  if(c1==1)
+  if(c1==1) // only if the graph is connected , the check for reverse edge is required else staright away its not strongly connected
     {
-      int c2=check(redge);
-      if(c2==1)
+      int c2=check(redge);  // check if the graph is connected according to the reverse edges
+      if(c2==1) 
 	res=1;
     }   
   
