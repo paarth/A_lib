508034e785d392e6832d50896e9caabdc4367223
paarth <paarth.dandiwala@gmail.com>
2012-07-16 00:53:01 +0530

    lps comment updated

--- Dynamic_Programming/LPS/LPS.cpp
+++ Dynamic_Programming/LPS/LPS.cpp
@@ -9,21 +9,20 @@
 
 using namespace std;
 
-string str;
-int dp[1000][1000];
+string str;                   // string to be operated upon          
 
 int lps(int s,int e)
 {
-  if(s>e)
+  if(s>e)                    // if lps query goes out of range
     return 0;
-  if(s==e)
+  if(s==e)                   // if only single character in lps query range
     return 1;
 
-  if(str[s]==str[e])
+  if(str[s]==str[e])         // if the first and last characters are same , append both in the solution and recurse for the substring excluding both
     {
-      return (2+lps(s+1,e-1));
+      return (2+lps(s+1,e-1));  
     }
-  else
+  else                        // if the first and last charcter are not same , then it is max of either excluding first or last
     {
       return max(lps(s+1,e),lps(s,e-1));
     }
