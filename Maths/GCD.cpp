28f640e246b7a6e45247c5057b9c2308e77ed8f7
paarth <paarth.dandiwala@gmail.com>
2012-11-28 20:10:14 +0530

    gcd: euclidean : rec & itr

--- Maths/GCD.cpp
+++ Maths/GCD.cpp
@@ -4,12 +4,23 @@
 
 using namespace std;
 
-long long gcd_(int a,int b)
+long long gcd_rec(int a,int b)
 {
   if(b==0)
     return a;
 
-  return gcd_(b,a%b);
+  return gcd_rec(b,a%b);
+}
+
+long long gcd_itr(long long a,long long b)
+{
+  while(b)
+    {
+      int r=a%b;
+      a=b;
+      b=r;
+    }
+  return a;
 }
 
 int main()
@@ -19,7 +30,8 @@ int main()
   cout<<"Enter the 2 numbers whose gcd needed: ";
   cin>>a>>b;
 
-  cout<<"GCD (a,b) = "<<gcd_(a,b)<<"\n";
+  cout<<"GCD (a,b) _Recursive := "<<gcd_rec(a,b)<<"\n";
+  cout<<"GCD (a,b) _Iterative := "<<gcd_itr(a,b)<<"\n";
 
   return 0;
 }
